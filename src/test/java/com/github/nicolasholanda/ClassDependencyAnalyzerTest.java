8ffaed0550f2e9da7b053569bd7191f9d7b42539
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-05-19 22:40:12 -0300

    Added unit tests

---
+++ src/test/java/com/github/nicolasholanda/ClassDependencyAnalyzerTest.java
@@ -0,0 +1,86 @@
+package com.github.nicolasholanda;
+
+import org.junit.jupiter.api.Test;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ClassDependencyAnalyzerTest {
+
+    @Test
+    void testAnalyzeSimpleClass() throws IOException {
+        String className = "com/github/nicolasholanda/SimpleClass";
+        String superName = "java/lang/Object";
+        File classFile = generateSimpleClass(className, superName);
+
+        ClassDependencyAnalyzer analyzer = new ClassDependencyAnalyzer();
+        analyzer.analyze(classFile);
+
+        assertEquals(className.replace('/', '.'), analyzer.getClassName());
+        Set<String> refs = analyzer.getReferencedClasses();
+        assertTrue(refs.contains(superName));
+    }
+
+    @Test
+    void testAnalyzeClassWithInterface() throws IOException {
+        String className = "com/github/nicolasholanda/WithInterface";
+        String superName = "java/lang/Object";
+        String[] interfaces = {"java/io/Serializable"};
+        File classFile = generateSimpleClass(className, superName, interfaces);
+
+        ClassDependencyAnalyzer analyzer = new ClassDependencyAnalyzer();
+        analyzer.analyze(classFile);
+
+        Set<String> refs = analyzer.getReferencedClasses();
+        assertTrue(refs.contains(superName));
+        assertTrue(refs.contains(interfaces[0]));
+    }
+
+    @Test
+    void testAnalyzeClassWithFieldAndMethod() throws IOException {
+        String className = "com/github/nicolasholanda/FieldMethodClass";
+        String superName = "java/lang/Object";
+        File file = File.createTempFile("FieldMethodClass", ".class");
+        file.deleteOnExit();
+
+        ClassWriter classWriter = new ClassWriter(0);
+
+        classWriter.visit(Opcodes.V11, Opcodes.ACC_PUBLIC, className, null, superName, null);
+        classWriter.visitField(Opcodes.ACC_PRIVATE, "list", "Ljava/util/List;", null, null).visitEnd();
+        classWriter.visitMethod(Opcodes.ACC_PUBLIC, "foo", "(Ljava/util/Map;)Ljava/util/List;", null, null).visitEnd();
+
+        classWriter.visitEnd();
+
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            fos.write(classWriter.toByteArray());
+        }
+
+        ClassDependencyAnalyzer analyzer = new ClassDependencyAnalyzer();
+        analyzer.analyze(file);
+
+        Set<String> refs = analyzer.getReferencedClasses();
+        assertTrue(refs.contains(superName));
+        assertTrue(refs.contains("java/util/List"));
+        assertTrue(refs.contains("java/util/Map"));
+    }
+
+    private File generateSimpleClass(String className, String superName, String... interfaces) throws IOException {
+        File file = File.createTempFile(className, ".class");
+        file.deleteOnExit();
+
+        ClassWriter classWriter = new ClassWriter(0);
+        classWriter.visit(Opcodes.V11, Opcodes.ACC_PUBLIC, className, null, superName, interfaces);
+        classWriter.visitEnd();
+
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            fos.write(classWriter.toByteArray());
+        }
+        return file;
+    }
+}
